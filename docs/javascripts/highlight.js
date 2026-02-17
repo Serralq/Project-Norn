8ad50fca5897dbd42f15ef7377b576db5592a407
Alexander Dao <alexanderngocdao@gmail.com>
2026-02-17 12:14:17 -0500

    Fixing the navigation.instant to also trigger highlight.js

--- docs/javascripts/highlight.js
+++ docs/javascripts/highlight.js
@@ -50,3 +50,64 @@ r.style.setProperty("--md-primary-fg-color--light", fgLight);
 r.style.setProperty("--md-primary-fg-color--dark", fgDark);
 r.style.setProperty("--md-primary-bg-color", bgColor);
 r.style.setProperty("--md-primary-bg-color--light", bgLight);
+
+// So it also works on navigation.instant
+document$.subscribe(function() {
+  console.log("Navigation triggered. Current Path:", window.location.pathname);
+  
+  var path = window.location.pathname;
+  var fgColor, fgLight, fgDark;
+
+  // Change based on page
+  if (path.match(/about/)) {
+    // Blue grey
+    fgColor = "#546d78";
+    fgLight = "#607c8a";
+    fgDark = "#455a63";
+  } else if (path.match(/travels/)) {
+    // Green
+    fgColor = "#4cae4f";
+    fgLight = "#68bb6c";
+    fgDark = "#398e3d";
+  } else if (path.match(/tech/)) {
+    // Deep purple
+    fgColor = "#7e56c2";
+    fgLight = "#9574cd";
+    fgDark = "#673ab6";
+  } else if (path.match(/stationary/)) {
+    // Brown
+    fgColor = "#795649";
+    fgLight = "#8d6e62";
+    fgDark = "#5d4037";
+  } else if (
+    path.match(/index/) ||
+    path.match(/ignore\.html/) ||
+    path === "/" ||
+    path === "/Project-Norn/" ||
+    path === "/tags.html"
+  ) {
+    // Indigo
+    console.log("indigo");
+    fgColor = "#4051b5";
+    fgLight = "#5d6cc0";
+    fgDark = "#303fa1";
+  } else {
+    // Default fallback
+    fgColor = "hsla(var(--md-hue),15%,9%,1)";
+    fgLight = "hsla(var(--md-hue),15%,9%,0.54)";
+    fgDark = "hsla(var(--md-hue),15%,9%,1)";
+  }
+
+  // Setting the colors
+  var r = document.querySelector(":root");
+  
+  // Constant colors (can define these outside the loop if you want, but inside is safer)
+  var bgColor = "#fff";
+  var bgLight = "#ffffffb3";
+
+  r.style.setProperty("--md-primary-fg-color", fgColor);
+  r.style.setProperty("--md-primary-fg-color--light", fgLight);
+  r.style.setProperty("--md-primary-fg-color--dark", fgDark);
+  r.style.setProperty("--md-primary-bg-color", bgColor);
+  r.style.setProperty("--md-primary-bg-color--light", bgLight);
+});
